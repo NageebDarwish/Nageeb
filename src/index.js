8baf9e3f277d2d915b6170b9bd8b62c6842c9c56
NageebDarwish <nageeb9696youtub@gmail.com>
2022-10-21 02:42:28 +0300

    updating loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./assest/all.css";
-import App from "./App";
+import LoadingSpinner from "./components/Loading";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min";
+const App = lazy(() => import("./App.js"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense
+      fallback={
+        <div>
+          {" "}
+          <LoadingSpinner />{" "}
+        </div>
+      }
+    >
+      <App />
+    </Suspense>
   </React.StrictMode>
 );
